b2abd8091caf126a9cbdee4a3f848672d6746d0c
Matheus Silveira <matheus.feitosa@usp.br>
2026-07-01 08:30:17 -0300

    feat: add week 11 - new projects post
    Signed-off-by: Matheus Silveira <matheus.feitosa@usp.br>

--- data.js
+++ data.js
@@ -10,6 +10,18 @@ window.SITE_DATA = {
     ],
     posts: [
         {
+            slug: "week11-new-projects",
+            title: "Week 11 - New Projects",
+            description: "After the last contribution to the Linux Kernel, we started the second phase of the class which consists of choosing a different FLOSS software to contribute. In this post, we detail the opportunities presented to us and our final choice for the next project. (I hope the icon doesn't spoiler anything)",
+            dateISO: "2026-06-24",
+            dateLabel: "24/06/2026",
+            category: "Week 11",
+            readingTime: "1 min read",
+            image: "https://kde.org/stuff/clipart/logo/kde-logo-gray-transparent-3000x3000.png",
+            imageBg: "#D95D39",
+            url: "posts/week11-new-projects"
+        },
+        {
             slug: "week10-kernel-fast-patching",
             title: "Week 10 - Speedrunning patches",
             description: "Following last week's spoiler, our first patch was accepted with an additional suggestion. Our simplification made part of the code unnecessary, so this week we removed that unused part.",
